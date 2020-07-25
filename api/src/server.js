5457892fb3f633b95a5d7f755bff121c1a6c5dd9
Samer Buna <samer@jscomplete.com>
2020-07-25 08:52:09 -0700

    5.1

--- api/src/server.js
+++ api/src/server.js
@@ -30,3 +30,14 @@ import * as config from './config';
 
   main();
 */
+
+import { graphql } from 'graphql';
+import { schema, rootValue } from './schema/index.js';
+
+const executeGraphQLRequest = async (request) => {
+  const resp = await graphql(schema, request, rootValue);
+
+  console.log(resp.data);
+};
+
+executeGraphQLRequest(process.argv[2]);
