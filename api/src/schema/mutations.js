9f9671c5266ae338b358e9279eababc118a02e65
Samer Buna <samer@jscomplete.com>
2020-08-09 21:07:09 -0700

    10.7

--- api/src/schema/mutations.js
+++ api/src/schema/mutations.js
@@ -3,6 +3,7 @@ import {
   GraphQLNonNull,
   GraphQLID,
 } from 'graphql';
+import { pubsub } from '../pubsub';
 
 import UserPayload from './types/payload-user';
 import TaskPayload from './types/payload-task';
@@ -46,7 +47,16 @@ const MutationType = new GraphQLObjectType({
         { input },
         { mutators, currentUser },
       ) => {
-        return mutators.taskCreate({ input, currentUser });
+        const { errors, task } = await mutators.taskCreate({
+          input,
+          currentUser,
+        });
+        if (errors.length === 0 && !task.isPrivate) {
+          pubsub.publish(`TASK_MAIN_LIST_CHANGED`, {
+            newTask: task,
+          });
+        }
+        return { errors, task };
       },
     },
     approachCreate: {
@@ -79,7 +89,16 @@ const MutationType = new GraphQLObjectType({
         { approachId, input },
         { mutators },
       ) => {
-        return mutators.approachVote({ approachId, input });
+        const { errors, approach } = await mutators.approachVote({
+          approachId,
+          input,
+        });
+        if (errors.length === 0) {
+          pubsub.publish(`VOTE_CHANGED_${approach.taskId}`, {
+            updatedApproach: approach,
+          });
+        }
+        return { errors, approach };
       },
     },
     userDelete: {
--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,5 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import morgan from 'morgan';
-import { graphqlHTTP } from 'express-graphql';
 import DataLoader from 'dataloader';
+import { ApolloServer } from 'apollo-server';
 
 import { schema } from './schema';
 import pgApiWrapper from './db/pg-api';
@@ -15,71 +11,65 @@ async function main() {
   const pgApi = await pgApiWrapper();
   const mongoApi = await mongoApiWrapper();
 
-  const server = express();
-  server.use(cors());
-  server.use(morgan('dev'));
-  server.use(bodyParser.urlencoded({ extended: false }));
-  server.use(bodyParser.json());
-  server.use('/:fav.ico', (req, res) => res.sendStatus(204));
+  const server = new ApolloServer({
+    schema,
+    formatError: (err) => {
+      const errorReport = {
+        message: err.message,
+        locations: err.locations,
+        stack: err.stack ? err.stack.split('\n') : [],
+        path: err.path,
+      };
+      console.error('GraphQL Error', errorReport);
+      return config.isDev
+        ? errorReport
+        : { message: 'Oops! Something went wrong! :(' };
+    },
+    context: async ({ req }) => {
+      const authToken =
+        req && req.headers && req.headers.authorization
+          ? req.headers.authorization.slice(7) // "Bearer "
+          : null;
+      const currentUser = await pgApi.userFromAuthToken(authToken);
+      if (authToken && !currentUser) {
+        throw Error('Invalid access token');
+      }
+      const loaders = {
+        users: new DataLoader((userIds) => pgApi.usersInfo(userIds)),
+        approachLists: new DataLoader((taskIds) =>
+          pgApi.approachLists(taskIds),
+        ),
+        tasks: new DataLoader((taskIds) =>
+          pgApi.tasksInfo({ taskIds, currentUser }),
+        ),
+        tasksByTypes: new DataLoader((types) =>
+          pgApi.tasksByTypes(types),
+        ),
+        searchResults: new DataLoader(async (searchTerms) =>
+          pgApi.searchResults({ searchTerms, currentUser }),
+        ),
+        detailLists: new DataLoader((approachIds) =>
+          mongoApi.detailLists(approachIds),
+        ),
+        tasksForUsers: new DataLoader((userIds) =>
+          pgApi.tasksForUsers(userIds),
+        ),
+      };
+      const mutators = {
+        ...pgApi.mutators,
+        ...mongoApi.mutators,
+      };
 
-  server.use('/', async (req, res) => {
-    const authToken =
-      req && req.headers && req.headers.authorization
-        ? req.headers.authorization.slice(7) // "Bearer "
-        : null;
-    const currentUser = await pgApi.userFromAuthToken(authToken);
-    if (authToken && !currentUser) {
-      return res.status(401).send({
-        errors: [{ message: 'Invalid access token' }],
-      });
-    }
-    const loaders = {
-      users: new DataLoader((userIds) => pgApi.usersInfo(userIds)),
-      approachLists: new DataLoader((taskIds) =>
-        pgApi.approachLists(taskIds),
-      ),
-      tasks: new DataLoader((taskIds) =>
-        pgApi.tasksInfo({ taskIds, currentUser }),
-      ),
-      tasksByTypes: new DataLoader((types) =>
-        pgApi.tasksByTypes(types),
-      ),
-      searchResults: new DataLoader(async (searchTerms) =>
-        pgApi.searchResults({ searchTerms, currentUser }),
-      ),
-      detailLists: new DataLoader((approachIds) =>
-        mongoApi.detailLists(approachIds),
-      ),
-      tasksForUsers: new DataLoader((userIds) =>
-        pgApi.tasksForUsers(userIds),
-      ),
-    };
-    const mutators = {
-      ...pgApi.mutators,
-      ...mongoApi.mutators,
-    };
-    graphqlHTTP({
-      schema,
-      context: { loaders, mutators, currentUser },
-      graphiql: { headerEditorEnabled: true },
-      customFormatErrorFn: (err) => {
-        const errorReport = {
-          message: err.message,
-          locations: err.locations,
-          stack: err.stack ? err.stack.split('\n') : [],
-          path: err.path,
-        };
-        console.error('GraphQL Error', errorReport);
-        return config.isDev
-          ? errorReport
-          : { message: 'Oops! Something went wrong! :(' };
-      },
-    })(req, res);
+      return { loaders, mutators, currentUser };
+    },
   });
 
-  server.listen(config.port, () => {
-    console.log(`Server URL: http://localhost:${config.port}/`);
-  });
+  server
+    .listen({ port: config.port })
+    .then(({ url, subscriptionsUrl }) => {
+      console.log(`Server URL: ${url}`);
+      console.log(`Subscriptions URL: ${subscriptionsUrl}`);
+    });
 }
 
 main();
